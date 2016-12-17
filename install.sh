68341e9e78ddfbeaa1a8eb30173958ea9ffbb4ff
Eli Finer <eli.finer@gmail.com>
2016-12-17 10:52:22 -0800

    aliases

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-curl https://raw.githubusercontent.com/gooli/linux-config/master/bashrc > ~/.bashrc
+curl https://raw.githubusercontent.com/gooli/linux-config/master/bash_aliases > ~/.bash_aliases
 curl https://raw.githubusercontent.com/gooli/linux-config/master/vimrc > ~/.vimrc
 curl https://raw.githubusercontent.com/gooli/linux-config/master/gitconfig > ~/.gitconfig
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
-source .bash_aliases
+source ~/.bash_aliases
