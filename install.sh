b5ab699e31e665894ac3b7a5fb8bc32f8d07b86f
Eli Finer <eli.finer@gmail.com>
2016-12-17 10:57:04 -0800

    cleaner progress

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-curl https://raw.githubusercontent.com/gooli/linux-config/master/bash_aliases > ~/.bash_aliases
-curl https://raw.githubusercontent.com/gooli/linux-config/master/vimrc > ~/.vimrc
-curl https://raw.githubusercontent.com/gooli/linux-config/master/gitconfig > ~/.gitconfig
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
+curl -# https://raw.githubusercontent.com/gooli/linux-config/master/bash_aliases > ~/.bash_aliases
+curl -# https://raw.githubusercontent.com/gooli/linux-config/master/vimrc > ~/.vimrc
+curl -# https://raw.githubusercontent.com/gooli/linux-config/master/gitconfig > ~/.gitconfig
+curl -# https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
 echo Please run "source ~/.bash_aliases" to complete install.
