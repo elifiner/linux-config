86b8c2ec5fe4858f094e318b7522019ea795cb4c
Eli Finer <eli.finer@gmail.com>
2017-02-02 17:17:55 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
 curl -# https://raw.githubusercontent.com/elifiner/linux-config/master/bash_aliases > ~/.bash_aliases
 curl -# https://raw.githubusercontent.com/elifiner/linux-config/master/vimrc > ~/.vimrc
 curl -# https://raw.githubusercontent.com/elifiner/linux-config/master/gitconfig > ~/.gitconfig
 curl -# https://raw.githubusercontent.com/elifiner/linux-config/master/authorized_keys > ~/.ssh/authorized_keys
 curl -# https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh > ~/.git-prompt.sh
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
 chmod 644 ~/.ssh/authorized_keys
 echo 'Please run "source ~/.bash_aliases" to complete install.'
